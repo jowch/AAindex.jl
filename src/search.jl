e4bb60a913ab554c1b50ea12e56259e37805dd58
Jonathan Chen <jwhc@ucla.edu>
2020-12-19 02:15:16 -0800

    Added docstrings for search functions

--- src/search.jl
+++ src/search.jl
@@ -7,11 +7,22 @@ const ENTRY_INDICES = [
 
 const PROVIDED_AAINDEX_DIRECTORY = joinpath(@__DIR__, "aaindex")
 
+"""
+    is_key(candidate)
 
+Checks if provided candidate key matches the format for AAindex accession
+numbers.
+"""
 function is_key(candidate::String)::Bool
     match(r"\w{4}\d{6}", candidate) !== nothing
 end
 
+"""
+    search_id(key, [aaindex_directory])
+
+Searches for specified AAindex entry key (accession number) and returns the
+loaded entry. Throws an error if the key is not in the database.
+"""
 function search_id(key::String, aaindex_directory=PROVIDED_AAINDEX_DIRECTORY)::AbstractAAIndex
     if is_key(key)
         try
@@ -22,6 +33,13 @@ function search_id(key::String, aaindex_directory=PROVIDED_AAINDEX_DIRECTORY)::A
     end
 end
 
+"""
+    search(term, [aaindex_directory])
+
+Performs a naive substring search for provided `term` in the key and
+title fields of records in the database. Returns an unsorted list of
+entries containing the term.
+"""
 function search(term::String, aaindex_directory=PROVIDED_AAINDEX_DIRECTORY)::Array{AbstractAAIndex}
     results = []
 
